574d00798146b8e2d8e906aaf8acae288af453dc
MichaelAlanAXL <michael.dtna@gmail.com>
2025-07-04 07:46:14 -0300

    feat: saved theme in localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,31 @@
 const toggleTheme = document.getElementById("toggleTheme");
 const rootHtml = document.documentElement
 
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme) {
+    rootHtml.setAttribute("data-theme", savedTheme);
+
+    if (savedTheme === "dark") {
+        toggleTheme.classList.add("bi-sun");
+        toggleTheme.classList.remove("bi-moon-stars");
+    } else {
+        toggleTheme.classList.add("bi-moon-stars");
+        toggleTheme.classList.remove("bi-sun");
+    }
+}
+
 function changeTheme() {
     const currentTheme = rootHtml.getAttribute("data-theme");
 
-    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme",   "dark")
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    rootHtml.setAttribute("data-theme", newTheme);
 
     toggleTheme.classList.toggle("bi-sun");
     toggleTheme.classList.toggle("bi-moon-stars");
+
+    // Salvar tema no localStorage
+    localStorage.setItem("theme", newTheme);
 }
 
 toggleTheme.addEventListener('click', changeTheme);
@@ -33,6 +51,15 @@ observer.observe(title);
 const skillsItems = document.querySelectorAll('.skills__item');
 skillsItems.forEach(item => observer.observe(item));
 
+const menuLinks = document.querySelectorAll(".menu__link");
+
+menuLinks.forEach(item => {
+    item.addEventListener('click', () => {
+        menuLinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    })
+})
+
 // função para pegar ano atual dinâmica
 function setCurrentYear() {
     const year = new Date().getFullYear();
@@ -40,3 +67,17 @@ function setCurrentYear() {
 }
 
 setCurrentYear();
+
+fetch("./data/projetos.json")
+    .then(response => response.json())
+    .then(projetos => {
+        const container = document.getElementById("");
+
+        projetos.forEach(projeto => {
+            const tituloElement = document.createElement("h3");
+            tituloElement.textContent = projeto.titulo;
+            tituloElement.ATTRIBUTE_NODE.classList.add("project-title");
+            container.appendChild(tituloElement);
+        });
+    })
+    .catch(error => console.error("Erro ao carregar JSON:", error));
